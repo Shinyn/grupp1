6828e208837289469dbea29f39523c4c73d2d886
darkshiira <darkshiirarhotmail.com>
2022-09-15 13:37:51 +0200

    mall

--- code.js
+++ code.js
@@ -2,5 +2,26 @@
 
 console.log("hello");
 //Hanna is here
-//adasdsd
 console.log("Hello to you too!");
+
+let kokbok = true
+while(kokbok = true)
+{ receptVal = prompt("Välj ett recept:");
+switch{ 
+    case "recept 1":
+        console.log("Du har valt recept 1");
+        break;
+
+        case "recept 2":
+            break;
+            case "recept 3":
+                break;
+                case "recept 4":
+                    break;
+                    case "recept 5":
+                        break;
+                        case "recept 6":
+                            break;
+        
+}
+}
