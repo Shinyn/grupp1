d18f67ebfd42b7981090b66a52512b5e2a90f571
darkshiira <darkshiirarhotmail.com>
2022-09-15 13:46:21 +0200

    Hanna namn

--- code.js
+++ code.js
@@ -10,22 +10,22 @@ while(kokbok = true)
 switch (prompt("Välj ett recept:"))
 {
     case "recept 1":
-        console.log("Du har valt recept 1");
+        console.log("Hannas recept:Du har valt recept 1");
         break;
 
-        case "recept 2":
+        case "Mattias recept 2":
         break;
 
-        case "recept 3":
+        case "Robins recept 3":
         break;
 
-        case "recept 4":
+        case "Dennis recept 4":
         break;
 
-        case "recept 5":
+        case "Eriks recept 5":
         break;
 
-        case "recept 6":
+        case "Linus recept 6":
         break;
 
     default:
